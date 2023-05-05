fd75729c25b010f3e2b562bf49c3d25e5ab31e99
Mike Pirog <mike@lando.dev>
2023-05-05 09:42:27 -0400

    first pass on setup-lando

---
+++ setup-lando.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const fs = require('fs');
+const get = require('lodash.get');
+const os = require('os');
+const path = require('path');
+const tc = require('@actions/tool-cache');
+
+const {execSync} = require('child_process');
+const {GitHub, getOctokitOptions} = require('@actions/github/lib/utils');
+const {paginateRest} = require('@octokit/plugin-paginate-rest');
+
+const getDownloadUrl = require('./lib/get-download-url');
+const getInputs = require('./lib/get-inputs');
+const getFileVersion = require('./lib/get-file-version');
+const resolveVersionSpec = require('./lib/resolve-version-spec');
+
+// const sleep = ms => {
+//   return new Promise(res => setTimeout(res, ms));
+// };
+
+const main = async () => {
+  // start by getting the inputs
+  const inputs = getInputs();
+
+  // reset some inputs to tests things
+  // inputs.landoVersionFile = '.tool-versions';
+  inputs.landoVersion = 'edge';
+
+  // show a warning if both version inputs are set
+  if (inputs.landoVersion && inputs.landoVersionFile) {
+    core.warning('Both lando-version and lando-version-file inputs are specified, only lando-version will be used');
+  }
+
+  // determine lando version spec to install
+  const spec = inputs.landoVersion || getFileVersion(inputs.landoVersionFile) || 'stable';
+  core.debug(`rolling with "${spec}" as version spec`);
+
+  // get a pagination vibed octokit so we can get release data
+  const Octokit = GitHub.plugin(paginateRest);
+  const octokit = new Octokit(getOctokitOptions(inputs.token));
+
+  // try/catch
+  try {
+    const releases = await octokit.paginate('GET /repos/{owner}/{repo}/releases', {owner: 'lando', repo: 'cli', per_page: 100});
+    core.debug(`found ${releases.length} valid releases`);
+
+    // attempt to resolve the spec
+    // @TODO: what about installing from source?
+    let version = resolveVersionSpec(spec, releases);
+    // throw error if we cannot resolve a version
+    if (!version) throw new Error(`Could not resolve "${spec}" into an installable version of Lando`);
+
+    // determine url of lando version to install
+    const downloadUrl = getDownloadUrl(version, inputs);
+    core.debug(`going to download version ${version} from ${downloadUrl}`);
+
+    // ensure needed RUNNER_ vars are set
+    // @NOTE: this is just to ensure we can run this locally
+    if (!get(process, 'env.RUNNER_TEMP', false)) process.env.RUNNER_TEMP = os.tmpdir();
+    if (!get(process, 'env.RUNNER_TOOL_CACHE', false)) process.env.RUNNER_TOOL_CACHE = os.tmpdir();
+
+    // @TODO:
+    // determine lando config to set?
+    // load in file if set, warn if file does not exist
+    // merge multiline config over config
+    // set the config as needed, also set some sort of githubactions thing
+    // query the config again to make sure it is set?
+
+    // download lando
+    // @NOTE: separate try catch here because we dont get a great error message from download tool
+    let landoPath;
+    try {
+      landoPath = await tc.downloadTool(downloadUrl);
+    } catch (error) {
+      throw new Error(`Unable to download Lando ${version} from ${downloadUrl}. ${error.message}`);
+    }
+
+    // make executable
+    fs.chmodSync(landoPath, '755');
+
+    // reset version information, we do this to get the source of truth on what we've downloaded
+    const output = execSync(`${landoPath} version`, {maxBuffer: 1024 * 1024 * 10, encoding: 'utf-8'});
+    version = output.split(' ').length === 2 ? output.split(' ')[1] : output.split(' ')[0];
+    core.debug(`downloaded lando is version ${version}`);
+
+    // move into the tool cache and compute path
+    const targetFile = inputs.os === 'Windows' ? 'lando.exe' : 'lando';
+    const toolDir = await tc.cacheFile(landoPath, targetFile, 'lando', version);
+    landoPath = path.join(toolDir, targetFile);
+    core.debug(`lando installed at ${landoPath}`);
+
+    // set the path and outputs
+    core.addPath(toolDir);
+    core.setOutput('lando-path', landoPath);
+
+    // test invoke
+    await exec.exec('lando', ['config']);
+
+  // catch unexpected
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+// main logix
+main();
