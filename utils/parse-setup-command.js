7fd5180f44718ba9891ee92e592bd56cabe6a014
Mike Pirog <mike@lando.dev>
2024-04-10 10:32:14 -0400

    relaxed restrictions on custom auto-setup command part 2

--- utils/parse-setup-command.js
+++ utils/parse-setup-command.js
@@ -4,7 +4,7 @@ module.exports = (command, landoBin = 'lando') => {
   // throw if not a string
   if (typeof command !== 'string') throw new Error('Setup command must be a string!');
   // validate a few things
-  if (command.includes('lando setup')) {
+  if (!command.includes('lando setup')) {
     throw new Error(`Setup command must include "lando setup"! You tried to run "${command}"`);
   }
 
