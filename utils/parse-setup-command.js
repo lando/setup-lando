5fa2904e3ea2486b9728ddb84dc5722517b0c901
Mike Pirog <mike@lando.dev>
2024-04-10 10:43:52 -0400

    relaxed restrictions on custom auto-setup command (#35)
    * relaxed restrictions on custom auto-setup command
    
    * relaxed restrictions on custom auto-setup command part 2
    
    * relaxed restrictions on custom auto-setup command part 3

--- utils/parse-setup-command.js
+++ utils/parse-setup-command.js
@@ -1,17 +1,13 @@
 'use strict';
 
-const {parseArgsStringToArgv} = require('string-argv');
-
-module.exports = command => {
+module.exports = (command, landoBin = 'lando') => {
   // throw if not a string
   if (typeof command !== 'string') throw new Error('Setup command must be a string!');
-  // parse string
-  command = parseArgsStringToArgv(command);
   // validate a few things
-  if (command[0] !== 'lando' || command[1] !== 'setup') {
-    throw new Error(`Setup command must begin with "lando setup"! You tried to run "${command.join(' ')}"`);
+  if (!command.includes('lando setup')) {
+    throw new Error(`Setup command must include "lando setup"! You tried to run "${command}"`);
   }
-  // remove first lando because we only care about the args
-  command.shift();
-  return command;
+
+  // return command but with lando invocations replaced with absolute paths to the landoBin
+  return command.replace(/lando /g, `${landoBin} `);
 };
