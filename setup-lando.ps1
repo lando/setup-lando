eb7d97a85c87535218e67906cb2f4b47473fb672
Aaron Feledy <aaron@arrowonesolutions.com>
2024-04-19 17:27:16 -0400

    Use prod urls in windows script

--- setup-lando.ps1
+++ setup-lando.ps1
@@ -47,9 +47,8 @@ param(
 )
 
 $LANDO_DEFAULT_MV = "3"
-#$LANDO_SETUP_PS1_URL = "https://raw.githubusercontent.com/lando/setup-lando/main/setup-lando.ps1"
-$LANDO_SETUP_PS1_URL = "https://raw.githubusercontent.com/lando/setup-lando/setup-windoze/setup-lando.ps1"
-$LANDO_SETUP_SH_URL = "https://raw.githubusercontent.com/lando/setup-lando/main/setup-lando.sh"
+$LANDO_SETUP_PS1_URL = "https://get.lando.dev/setup-lando.ps1"
+$LANDO_SETUP_SH_URL = "https://get.lando.dev/setup-lando.sh"
 $LANDO_APPDATA = "$env:LOCALAPPDATA\Lando"
 
 $issueEncountered = $false
