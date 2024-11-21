1979504894dba3c50a1cd615a495e54f1ce7d6ab
Mike Pirog <mike@lando.dev>
2024-11-21 09:25:40 -0500

    add path based installation to posix install script (#71)
    * add path based installation to posix install script
    
    * add path based installation to posix install script part 2
    
    * add path based installation to posix install script part 3
    
    * add path based installation to posix install script part 4

--- setup-lando.sh
+++ setup-lando.sh
@@ -507,11 +507,18 @@ elif [[ "${VERSION}" == "3-dev" ]] || [[ "${VERSION}" == "3-latest" ]]; then
   URL="https://files.lando.dev/core/lando-${OS}-${ARCH}-dev"
   VERSION_DEV=1
 
-# CUSTOM
-else
-  if [[ $VERSION != v* ]]; then
-    VERSION="v${VERSION}"
+# CUSTOM VERSION
+elif [[ ! -f "$VERSION" ]] && [[ $VERSION != v* ]]; then
+  VERSION="v${VERSION}"
+
+# PATH VERSION
+elif [[ -f "${VERSION}" ]]; then
+  if [[ "${VERSION}" == /* ]]; then
+    URL=file://$VERSION
+  else
+    URL=file://$(pwd)/$VERSION
   fi
+  VERSION="$($VERSION version)"
 fi
 
 # Set some helper things
@@ -525,23 +532,29 @@ else
   HRV="$VERSION"
 fi
 
-# set url depending on non-dev LMVs
-if [[ $LMV == '3' ]] && [[ -z "${VERSION_DEV-}" ]]; then
-  URL="https://github.com/lando/core/releases/download/${VERSION}/lando-${OS}-${ARCH}-${VERSION}"
-elif [[ $LMV == '4' ]] && [[ -z "${VERSION_DEV-}" ]]; then
-  URL="https://github.com/lando/core-next/releases/download/${VERSION}/lando-${OS}-${ARCH}-${VERSION}"
+# if URL is still not set at this point we assume its a github version download
+if [[ -z "${URL-}" ]]; then
+  if [[ $LMV == '3' ]] && [[ -z "${VERSION_DEV-}" ]]; then
+    URL="https://github.com/lando/core/releases/download/${VERSION}/lando-${OS}-${ARCH}-${VERSION}"
+  elif [[ $LMV == '4' ]] && [[ -z "${VERSION_DEV-}" ]]; then
+    URL="https://github.com/lando/core-next/releases/download/${VERSION}/lando-${OS}-${ARCH}-${VERSION}"
+  fi
+fi
+
+# abort if we have no URL at this point
+if [[ -z "${URL-}" ]]; then
+  abort "could not resolve '${ORIGINAL_VERSION}' to a file or URL!"
+else
+  debug "resolved v${LMV} version '${ORIGINAL_VERSION}' to ${VERSION} (${URL})"
 fi
 
 # autoslim all v3 urls by default
-# @TODO: --fat flag to stop this?
-if [[ $LMV == '3' ]] && [[ $FAT != '1' ]]; then
+# @TODO: restrict this to 3 < 3.24.0 at some point?
+if [[ $URL != file://* ]] && [[ $LMV == '3' ]] && [[ $FAT != '1' ]]; then
   URL="${URL}-slim"
   HRV="$VERSION-slim"
   debug "autoslimin url for lando 3"
 fi
-
-# debug version resolution
-debug "resolved v${LMV} version '${ORIGINAL_VERSION}' to ${VERSION} (${URL})"
 
 # force setup to 0 if lando 4
 if [[ $SETUP == '1' ]] && [[ $LMV == '4' ]]; then
@@ -767,14 +780,21 @@ cd "/usr" || exit 1
 if [[ -z "${NONINTERACTIVE-}" ]]; then
   log "${tty_bold}this script is about to:${tty_reset}"
   log
+
   # sudo prompt
   if needs_sudo; then log "- ${tty_green}prompt${tty_reset} for ${tty_bold}sudo${tty_reset} password"; fi
+
   # download
-  log "- ${tty_magenta}download${tty_reset} lando ${tty_bold}${HRV}${tty_reset} to ${tty_bold}${DEST}${tty_reset}"
+  if [[ $URL != file://* ]]; then log "- ${tty_magenta}download${tty_reset} lando ${tty_bold}${HRV}${tty_reset} to ${tty_bold}${DEST}${tty_reset}"
+  # or move
+  else log "- ${tty_magenta}move${tty_reset} lando ${tty_bold}${ORIGINAL_VERSION}${tty_reset} to ${tty_bold}${DEST}${tty_reset}"; fi
+
   # setup
   if [[ "$SETUP" == "1" ]]; then log "- ${tty_blue}run${tty_reset} ${tty_bold}lando setup${tty_reset}"; fi
+
   # shellenv
   log "- ${tty_blue}run${tty_reset} ${tty_bold}lando shellenv --add${tty_reset}"
+
   # block for user
   wait_for_user
 fi
