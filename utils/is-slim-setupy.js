904c42cd17d3ce1aa8033a6f78546bb18f40f8eb
Mike Pirog <mike@lando.dev>
2024-12-06 20:23:56 -0500

    remove lando setup and slim support for lando >=3.24 part 3

--- utils/is-slim-setupy.js
+++ utils/is-slim-setupy.js
@@ -5,7 +5,7 @@ const {s3Releases} = require('./get-convenience-aliases');
 
 module.exports = version => {
   // slim can be any v3 s3 alias
-  if (s3Releases.includes(version)) return version.split('-')[0] === '3';
+  if (s3Releases.includes(version)) return false;
   // or anything in the range
-  return satisfies(version, '>3.20 <3.24.0', {includePrerelease: true, loose: true});
+  return satisfies(version, '>3.20 <=3.23.9999', {includePrerelease: true, loose: true});
 };
